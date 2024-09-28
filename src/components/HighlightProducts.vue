1da51c35893c13123281c939d2f24238c28b5db6
heloisa-lopes <heloisalopesalbano@gamil.com>
2024-09-28 16:03:30 -0300

    first commit

---
+++ src/components/HighlightProducts.vue
@@ -0,0 +1,5 @@
+<script setup>
+</script>
+<template>
+    highlightProducts
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+<header>
+  <Navbar />
+</header>
+<main>
+  <TopSection />
+  <CategoryCard />
+</main>
+<footer>
+</footer>
+</template>
